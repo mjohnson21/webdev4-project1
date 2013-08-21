f49da778d421a8065c60b1a20c336544136a490a
mjohnson21 <mjohnson21@gmail.com>
2013-08-21 16:09:23 -0400

    php changes

---
+++ includes/navbar.php
@@ -0,0 +1,18 @@
+<div class="navbar navbar-inverse navbar-fixed-top">
+    <div class="navbar-inner">
+      <div class="container">
+        <a class="brand" href="./index.html">JEEP</a>
+        <ul class="nav">
+          <li class="active">
+            <a href="./index.html">Home</a>
+          </li>
+          <li><a href="vehicles.html">Vehicles</a></li>
+          <li><a href="tools.html">Shopping Tools</a></li>
+          <li><a href="capability.html">Capabilty</a></li>
+          <li><a href="owners.html">Owners</a></li>
+        </ul>
+        <a href="#" class="build-price">Build and Price</a>
+      </div>
+    </div>
+  </div>
+  <!-- END HEADER -->
